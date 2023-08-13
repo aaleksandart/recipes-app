e128fd25060e2eb3235caf47113b8ede96b82f5e
Aleksandar Todorovic <90002225+aaleksandart@users.noreply.github.com>
2023-08-14 00:25:53 +0200

    start
    first commit

--- src/app/components/contentArea.tsx
+++ src/app/components/contentArea.tsx
@@ -13,14 +13,8 @@ export default function ContentArea(content: IContent) {
         <>
             <div className="bg-white h-64 w-full max-w-sm p-4 rounded-lg shadow-lg md:h-80 xl:h-96 max-w-lg">
                 <span className={`underline px-2 ${content.bgColor} marker-font text-2xl`}>{content.name}</span>
-                {
-                    // content.recipes.map(r => (
-                    //     <Link key={rec.id}>
-                    //         <Customer member={customer}/>
-                    //     </Link>
-                    // ))
-                }
-                <Btn text='Explore' size="w-36"/>
+
+                <Btn text='Explore' size="w-36" />
             </div>
         </>
     )
